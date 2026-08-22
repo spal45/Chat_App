7d9661b6f51436bfd65424d1b21a9bcdf2341612
Subhankar Pal <subhankar.pal45@gmail.com>
2026-08-22 09:56:46 +0530

    Initial Setup

---
+++ backend/user/src/config/rabbitmq.ts
@@ -0,0 +1,41 @@
+import ampq from 'amqplib'
+
+let channel: ampq.Channel;
+
+export const connectRabbitMQ = async ()=> {
+    try{
+        const hostname = process.env.Rabbitmq_Host;
+        const username = process.env.Rabbitmq_Username;
+        const password = process.env.Rabbitmq_Password;
+
+        if (!hostname || !username || !password) {
+            throw new Error('Missing RabbitMQ configuration');
+        }
+
+        const connection = await ampq.connect({
+            protocol: "amqp",
+            hostname,
+            port: 5672,
+            username,
+            password
+        })
+
+        channel = await connection.createChannel();
+        console.log("✅ Connected to rabbitmq");
+
+    }catch(error){
+        console.log('Failed to connect to rabbitmq', error)
+    }
+}
+
+export const publishToQueue = async (queueName:string, message: any) =>{
+    if(!channel) {
+        console.log("Rabbitmq channel is not initalizes");
+    }
+
+    await channel.assertQueue(queueName, { durable: true });
+
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true
+    })
+}
